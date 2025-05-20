75aaf6266b504812d8921bb83d9cae15426a43b2
gilhad <gilhad@seznam.cz>
2025-05-21 01:32:05 +0200

    forth2inc.py: words.4th -> words.inc

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define VERSION_STRING "0.0.1-2-gb0539a1++"
-#define VERSION_COMMIT "b0539a1"
-#define VERSION_MESSAGE "merging, merging ... macros, STR_ings, colors, bugs ..."
+#define VERSION_STRING "0.0.1-3-gff5e1b7++"
+#define VERSION_COMMIT "ff5e1b7"
+#define VERSION_MESSAGE "Fixed stack memory leaks, ascii, foto"
